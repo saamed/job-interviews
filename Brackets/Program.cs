5918f7640db8146e0695e74181aa24f807fea1f3
Bartosz Cłapa <bclapa@gmail.com>
2021-10-05 23:28:12 +0200

    added Brackets tech exercise

---
+++ Brackets/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsValid("()")); // true
+            Console.WriteLine(IsValid(")()")); // false
+            Console.WriteLine(IsValid("()()")); //true
+            Console.WriteLine(IsValid("())(")); //false
+            Console.WriteLine(IsValid("(()())")); //true
+            Console.WriteLine(IsValid("(())())")); //false
+            Console.WriteLine(IsValid("((()()))")); //true
+            Console.WriteLine(IsValid("((()(()))")); //false
+        }
+
+        private static bool IsValid(string input)
+        {
+            var openings = 0;
+            
+            foreach (var c in input)
+            {
+                if (c == '(')
+                {
+                    openings++;
+                    continue;
+                }
+
+                if (c == ')')
+                {
+                    openings--;
+                }
+
+                if (openings < 0)
+                    return false;
+            }
+
+            return openings == 0;
+        }
+    }
+}
